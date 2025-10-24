c18bb269f4570bd7650bf8be9f5d19433243ccb1
jjwesty7 <143133485+jjwesty7@users.noreply.github.com>
2025-10-24 03:32:30 -0600

    Update to the dialogue system
    Attempted to make some helpful changes to the dialogue system.
    
    Printer now takes list of strings to print rather than just having a singular string hard coded.
    
    Carry on conversation by pressing E.
    
    Need to work out some bugs like having to press keys twice to accomplish desired task.
    
    Want to finish implementing feature to speed through conversation and have prompt appear when close enough to NPC rather than just appear directly on the screen from the start.
    
    Finally, need to clean and organize code a little.

--- Assets/Scripts/NPCSystem.cs
+++ Assets/Scripts/NPCSystem.cs
@@ -1,30 +1,48 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NPCSystem : MonoBehaviour
 {
     bool playerDetection = false;
     public TextPrinter textPrinter;
+    List<string> gpaDialogue = new List<string>();
+
+    string line1 = "Hey kiddo! Lotta bodies in the lake again. Gett'n to be that time o' year I guess.";
+    string line2 = "Say, that reminds me of when they found your gam gam in there.";
+    string line3 = "Anywho... them bodies ain't gon' fish themselves out. Better get a move on.";
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        /*gpaDialogue.Add(line1);
+        gpaDialogue.Add(line2);
+        gpaDialogue.Add(line3);*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(/*playerDetection && */Input.GetKeyDown(KeyCode.F)) {
+        if(playerDetection && Input.GetKeyDown(KeyCode.F)) {
+            gpaDialogue.Add(line1);
+            gpaDialogue.Add(line2);
+            gpaDialogue.Add(line3);
             print("Dialogue started!");
-            textPrinter.PrintText("Hiya! Did you see the lake lately? It's been getting mighty dirty. What a shame.");
+            textPrinter.Print(gpaDialogue, OnDialogueFinished);
         }
     }
 
     private void OnTriggerEnter(Collider other) {
-        if(other.name == "Player") {
+        if(other.tag == "Player") {
             playerDetection = true;
         }
     }
 
     private void OnTriggerExit(Collider other) {
         playerDetection = false;
+        gpaDialogue.Clear();
+    }
+
+    private void OnDialogueFinished() {
+        print("Grandpa finished talking!");
     }
 }
--- Assets/Scripts/TextPrinter.cs
+++ Assets/Scripts/TextPrinter.cs
@@ -1,26 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using TMPro;
 
 public class TextPrinter : MonoBehaviour
 {
 
-    private TMP_Text subtitleTextMesh;
+    [SerializeField][Range(0,0.5f)] float normalTextSpeed;
+    [SerializeField][Range(0,0.5f)] float skipTextSpeed;
+
+    float currentTextSpeed;
+
+    private TMP_Text textMesh;
     private Coroutine typingCoroutine;
 
     void Awake()
     {
-        subtitleTextMesh = GetComponent<TMP_Text>();
+        textMesh = GetComponent<TMP_Text>();
     }
 
-    public void PrintText(string line) {
+    /*public void PrintText(string line) {
         if(typingCoroutine != null) {
             StopCoroutine(typingCoroutine);
         }
 
         typingCoroutine = StartCoroutine(TypeTextCO(line));
+    }*/
+
+    public void Print(List<string> sentences, Action onFinishedPrinting)
+    {
+        if(typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+        }
+
+        typingCoroutine = StartCoroutine(PrintDialogue(sentences, onFinishedPrinting));
     }
+
+    IEnumerator PrintDialogue(List<string> sentences, Action onFinishedPrinting)
+    {
+        foreach(var sentence in sentences)
+        {
+            textMesh.text = string.Empty;
+            yield return new WaitForSeconds(0.1f);
+
+            foreach(var letter in sentence)
+            {
+                HandleTextSpeed();
+                textMesh.text += letter;
+                if(letter == ' ') continue;
+                yield return new WaitForSecondsRealtime(currentTextSpeed);
+            }
+            yield return new WaitUntil(() => Input.GetKeyUp(KeyCode.E));
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.E));
+        }
+        textMesh.text = string.Empty;
+        onFinishedPrinting?.Invoke();
+    }
+
+    /*void RepositionSentence(string sentence)
+    {
+        textMesh.text = sentence;
+        textMesh.ForceMeshUpdate();
+        var firstChar = textMesh.textInfo.lineInfo[0].firstVisibleCharacterIndex;
+        var lastChar = textMesh.textInfo.lineInfo[0].lastVisisbleCharacterIndex;
+        var firstCharPos = textMesh.textInfo.characterInfo[firstChar].topLeft;
+        var lastCharPos = textMesh.textInfo.characterInfo[lastChar].topRight;
+        textMesh.rectTransfrom.anchoredPosition = new Vector2(0 - ((firstCharPos.x + lastCharPos.x) / 2), textMesh.reactTransform.anchoredPosition);
+        textMesh.text = string.Empty;
+    }*/
+
+    void HandleTextSpeed()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+
+            currentTextSpeed = skipTextSpeed;
+        }
+        else{
+            currentTextSpeed = normalTextSpeed;
+        }
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -28,11 +90,11 @@ public class TextPrinter : MonoBehaviour
 
     private IEnumerator TypeTextCO(string textToType)
     {
-        subtitleTextMesh.text = string.Empty;
+        textMesh.text = string.Empty;
 
         for(int i = 0; i < textToType.Length; i++)
         {
-            subtitleTextMesh.text += textToType[i];
+            textMesh.text += textToType[i];
             yield return new WaitForSeconds(0.025f);
         }
     }
