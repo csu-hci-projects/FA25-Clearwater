c3a24ac1c942a5dd2e351904bcb3fb94f66df70b
Lucas Hill <lucas.hill@colostate.edu>
2025-11-03 17:10:47 -0700

    AI initial implementation

--- Assets/LucasStuff/DotsAndBoxes/HeuristicAI.cs
+++ Assets/LucasStuff/DotsAndBoxes/HeuristicAI.cs
@@ -1,4 +1,55 @@
 public class HeuristicAI
 {
-    
+    private readonly Random random = new();
+
+    public Edge ChooseMove(GameBoard board)
+    {
+        Edge? bestMove = null;
+        int bestScore = int.MinValue;
+
+        foreach (var move in board.GetAvailableMoves())
+        {
+            int score = EvaluateMove(board, move);
+            if (score > bestScore)
+            {
+                bestScore = score;
+                bestMove = move;
+            }
+        }
+
+        return bestMove ?? throw new Exception("AI panicked!");
+    }
+
+    private int EvaluateMove(GameBoard board, Edge move)
+    {
+        int score = 0;
+
+        foreach (var (row, col) in board.GetAffectedBoxes(move))
+        {
+            score += random.Next(-5, 6);
+            switch (CountBoxEdges(board, box.row, box.col, move))
+            {
+                case 1:
+                    score += 3; break;
+                case 2:
+                    score -= 10; break;
+                case 3:
+                    score += 25; break;
+            }
+        }
+
+        return score;
+    }
+
+    private int CountBoxEdges(GameBoard board, int row, int col, Edge hypotheticalMove)
+    {
+        int count = 0;
+
+        if (board.HasEdge(row, col, EdgeType.Horizontal, true, hypotheticalMove)) count++;
+        if (board.HasEdge(row + 1, col, EdgeType.Horizontal, false, hypotheticalMove)) count++;
+        if (board.HasEdge(row, col, EdgeType.Vertical, true, hypotheticalMove)) count++;
+        if (board.HasEdge(row, col + 1, EdgeType.Vertical, false, hypotheticalMove)) count++;
+
+        return count;
+    }
 }
