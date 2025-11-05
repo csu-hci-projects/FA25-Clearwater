20c6eb1009092c03e5aee35ada4abeb2cb7def1c
Lucas Hill <lucas.hill@colostate.edu>
2025-11-04 23:29:26 -0700

    moved animated-old-man-character

--- Assets/LucasStuff/DotsAndBoxes/Scripts/Controller.cs
+++ Assets/LucasStuff/DotsAndBoxes/Scripts/Controller.cs
@@ -5,11 +5,13 @@ public class Controller : MonoBehaviour
 {
     [SerializeField] CinemachineCamera mainCam;
     [SerializeField] Camera gameCam;
+    private bool gaming;
 
     void Awake()
     {
         mainCam.enabled = true;
         gameCam.enabled = false;
+        gaming = false;
     }
 
     void Update()
